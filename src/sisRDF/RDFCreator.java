4a3c55b79316a09e1c11acb717761d182f1641c0
VictorSanchez94 <vs_zgz94@hotmail.com>
2016-02-07 18:04:46 +0100

    a

--- src/sisRDF/RDFCreator.java
+++ src/sisRDF/RDFCreator.java
@@ -255,7 +255,7 @@ public class RDFCreator {
 				+ "?doc dc:date ?date."
 				+ "?doc gr12:keyword ?key."
 				//Todo: solo falta esto y lo tenemos
-				//+ "?key rdf:about \"http://RecInfo/gr12/Tesauro#videojuego\"."
+				+ "?key skos:inScheme \"http://RecInfo/gr12/Tesauro#videojuego\"."
 				+ "FILTER (?date > \"2009\")}";
 		
 		//ejecutamos la consulta y obtenemos los resultados
@@ -271,6 +271,45 @@ public class RDFCreator {
 		      Resource key = soln.getResource("key");
 		      System.out.println(doc.asResource().getURI() + " " + date.toString());
 		      System.out.println(key);
+		    }
+		  } finally { qexec.close() ; }
+	}
+	
+	public static void ejecutarConsultaPrueba() {
+//		String queryString = "PREFIX gr12: <http://recInfo/gr12/terms/>"
+//		+ "PREFIX skos: <http://RecInfo/gr12/Tesauro#>"
+//		+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+//		+ "PREFIX dc: <http://purl.org/dc/elements/1.1/>"
+//		+ "Select ?doc ?date ?key WHERE {"
+//				+ "?doc dc:date ?date."
+//				+ "?doc gr12:keyword ?key."
+//				//Todo: solo falta esto y lo tenemos
+//				+ "?key skos:inScheme \"http://RecInfo/gr12/Tesauro#videojuego\"."
+//				+ "FILTER (?date > \"2009\")}";
+		
+		String queryString = "PREFIX gr12: <http://recInfo/gr12/terms/>"
+				+ "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
+				+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+				+ "PREFIX dc: <http://purl.org/dc/elements/1.1/>"
+				+ "SELECT ?a WHERE {"
+					+ "?a skos:broader \"http://RecInfo/gr12/Tesauro\"}";
+
+		
+		//ejecutamos la consulta y obtenemos los resultados
+		  Query query = QueryFactory.create(queryString) ;
+		  QueryExecution qexec = QueryExecutionFactory.create(query, model) ;
+		  try {
+		    ResultSet results = qexec.execSelect() ;
+		    for ( ; results.hasNext() ; )
+		    {
+		      QuerySolution soln = results.nextSolution() ;
+		      Resource a = soln.getResource("a");
+//		      Resource doc = soln.getResource("doc");  
+//		      Literal date = soln.getLiteral("date");
+//		      Resource key = soln.getResource("key");
+//		      System.out.println(doc.asResource().getURI() + " " + date.toString());
+//		      System.out.println(key);
+		      System.out.println(a);
 		    }
 		  } finally { qexec.close() ; }
 	}
@@ -292,7 +331,7 @@ public class RDFCreator {
 			e.printStackTrace();
 		}
         System.out.println("\n\n===============================================================================\n\n");
-        ejecutarConsulta4();
+        ejecutarConsultaPrueba();
     }
 	
 }
